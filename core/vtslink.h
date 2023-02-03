bfd64128de6ed7e99c088f616bcd7cb8c70f21b4
reito <cnschwarzer@qq.com>
2023-02-03 11:58:19 +0800

    revert 1080

--- core/vtslink.h
+++ core/vtslink.h
@@ -4,8 +4,8 @@
 #define VTSLINK_HTTP_BASEURL "https://vts.reito.fun"
 #define VTSLINK_WS_BASEURL "wss://vts.reito.fun" 
 
-#define VTSLINK_FRAME_WIDTH 1600
-#define VTSLINK_FRAME_HEIGHT 900
+#define VTSLINK_FRAME_WIDTH 1920
+#define VTSLINK_FRAME_HEIGHT 1080
 #define VTSLINK_FRAME_D 1
 #define VTSLINK_FRAME_N 60
 
